fc584ed95198ad224694ceedb42eef40a21c656b
Boksh <bokshitsky@gmail.com>
2016-02-21 15:20:44 +0300

    Initial commit

---
+++ src/main/java/httpserver/main.java
@@ -0,0 +1,10 @@
+package httpserver;
+
+
+public class main {
+
+    public static void main(String [] args) {
+        System.out.print("sasss");
+    }
+
+}
---
+++ test_grep.sh
@@ -0,0 +1,4 @@
+mvn clean package
+java -jar target/run_all_greps.jar "Iron Throne" test_data/*
+
+echo "Ready!"
